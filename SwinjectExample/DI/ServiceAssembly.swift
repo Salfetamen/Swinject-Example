ebd5e6b8045680f5b2b4e94beafe6419461ce3ae
Stashevskiy Andrey Sergeevich <a.stashevskiy@tinkoff.ru>
2019-07-25 23:48:45 +0300

    assembly

--- SwinjectExample/DI/ServiceAssembly.swift
+++ SwinjectExample/DI/ServiceAssembly.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import Swinject
+
+
+final class ServiceAssembly: Assembly {
+    
+    func assemble(container: Container) {
+        container.register(FirstServiceProtocol.self) { _ in
+            FirstService()
+        }
+        
+        container.register(SecondServiceProtocol.self) { r in
+            let first = r.resolve(FirstServiceProtocol.self)!
+            let second = SecondService(first: first)
+            
+            return second
+        }
+    }
+}
