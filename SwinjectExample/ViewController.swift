ea1853083df2b78c91a635bf73f4e285b2c36e1c
Stashevskiy Andrey Sergeevich <a.stashevskiy@tinkoff.ru>
2019-07-25 23:38:34 +0300

    initial commit

---
+++ SwinjectExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwinjectExample
+//
+//  Created by a.stashevskiy on 25/07/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
